47836a87d30b08a6c32924ed30c383e814d6e943
aazar412 <aazar412@gmail.com>
2022-12-26 11:20:16 -0500

    Finished code for task 1

--- src/Tasks/Odd_Even.java
+++ src/Tasks/Odd_Even.java
@@ -1,5 +1,24 @@
 package Tasks;
 
-public class Odd_Even {
+import java.util.Scanner;
 
+public class Odd_Even {
+    /*
+    Write a method to identify if the number is even or odd
+     */
+    public static void main(String[] args) {
+
+
+        Scanner number = new Scanner(System.in);
+        System.out.println("Please enter number");
+        int numberInQuestion= number.nextInt();
+
+        if (numberInQuestion%2==0){
+            System.out.println("Number is Even");
+        }else{
+            System.out.println("Number is Odd");
+        }
+
+        number.close();
+    }
 }
