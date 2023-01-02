38cc41acf42bcec6a39198d459d673c64940c5c1
aazar412 <aazar412@gmail.com>
2023-01-02 13:01:51 -0500

    swapping task finished

--- src/Tasks/Swapping.java
+++ src/Tasks/Swapping.java
@@ -1,7 +1,26 @@
 package Tasks;
 
 public class Swapping {
+    public static void main(String[] args) {
+        swapping(4,6);
+
+    }
+
+
+        public static void swapping ( int a, int b){
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            System.out.println("a: "+a);
+            System.out.println("b: "+b);
+        }
+
+
+
+
+
     /*
     Swap two variables without using a third variable
      */
-}
+    }
